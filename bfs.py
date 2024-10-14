db6d9b5a4a1cc18c2ddf43aa23e66c204bcc905e
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-10-15 01:08:02 +0530

    bfs

--- bfs.py
+++ bfs.py
@@ -1,53 +1,26 @@
+def bfs(graph, start_node):
+    queue = deque([start_node])
+    visited = {start_node}
+
+    while queue:
+        node = queue.popleft()
+        print(node, end=" ")
+
+        for i in graph[node]:
+            if i not in visited:
+                visited.add(i)
+                queue.append(i)
+
+
+
 graph = {
-    'A': set(['B', 'C']),
-    'B': set(['A', 'D', 'E']),
-    'C': set(['A', 'F']),
-    'D': set(['B']),
-    'E': set(['B', 'F']),
-    'F': set(['C', 'E'])
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': [],
+    'F': []
 }
 
+bfs(graph, 'A')
 
-def bfs(start):
-    queue = [start]
-    levels = {}  
-    levels[start] = 0  
-    visited = set(start)
-
-    while queue:
-        node = queue.pop(0)
-        neighbours = graph[node]
-        for neighbor in neighbours:
-            if neighbor not in visited:
-                queue.append(neighbor)
-                visited.add(neighbor)
-                levels[neighbor] = levels[node] + 1
-
-    print(levels)
-    return visited
-
-print(str(bfs('A'))) 
-
-
-def bfs_paths(graph, start, goal):
-    queue = [(start, [start])]
-    while queue:
-        (vertex, path) = queue.pop(0)
-        for next in graph[vertex] - set(path):
-            if next == goal:
-                yield path + [next]
-            else:
-                queue.append((next, path + [next]))
-
-result = list(bfs_paths(graph, 'A', 'F'))
-print(result)  
-
-
-def shortest_path(graph, start, goal):
-    try:
-        return next(bfs_paths(graph, start, goal))
-    except StopIteration:
-        return None
-
-result1 = shortest_path(graph, 'A', 'F')
-print(result1)  
