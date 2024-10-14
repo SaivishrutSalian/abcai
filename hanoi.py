353dd724ea3e5963d6a652b950aacb6752648b1f
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-10-15 02:28:03 +0530

    .

--- hanoi.py
+++ hanoi.py
@@ -1,11 +1,13 @@
-def moveTower(height, fromPole, toPole, withPole):
-    if height >= 1:
-        moveTower(height - 1, fromPole, withPole, toPole)
-        moveDisk(fromPole, toPole)
-        moveTower(height - 1, withPole, toPole, fromPole)
+def solve(n, source, target, auxillary):
+    if n == 1:
+        print(f"Move disk 1 from {source} to {target}")
+        return
+    
+    solve(n-1, source, auxillary, target)
+    print(f"Move disk {n} from {source} to {target}")
 
-def moveDisk(fp, tp):
-    print("Moving disk from", fp, "to", tp)
+    solve(n-1, auxillary, target, source)
 
 
-moveTower(3, "A", "B", "C")
+n = 3
+solve(n, 'A', 'C', 'B')
