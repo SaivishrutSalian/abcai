51e2207c620adb97bd61b3b02de9c92a5f92ed8d
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-10-15 08:34:02 +0530

    .

--- waterJug.py
+++ waterJug.py
@@ -1,103 +1,68 @@
-capacity = (12, 8, 5)  
-x = capacity[0]  
-y = capacity[1]  
-z = capacity[2]  
+from collections import deque
 
-
-memory = {}
-
-ans = []
-
-def get_all_states(state):
+def water_jug():
     
-    a = state[0]  
-    b = state[1]  
-    c = state[2] 
+    # initial state -> ( jug A, jug B, jug C)
+    initial_state = (12, 0, 0)
+    goal = 6
+    queue = deque([initial_state])
+    visited = []
+    path = []
 
-    
-    if a == 6 and b == 6:
-        ans.append(state)
-        return True
 
-    
-    if (a, b, c) in memory:
-        return False
-    
-    
-    memory[(a, b, c)] = 1
-
-    
-    if a > 0:
+    while queue:
+        state = queue.popleft()
+        a, b, c = state
         
-        if a + b <= y:
-            if get_all_states((0, a + b, c)):
-                ans.append(state)
-                return True
-        else:
-            if get_all_states((a - (y - b), y, c)):
-                ans.append(state)
-                return True
-        
-        
-        if a + c <= z:
-            if get_all_states((0, b, a + c)):
-                ans.append(state)
-                return True
-        else:
-            if get_all_states((a - (z - c), b, z)):
-                ans.append(state)
-                return True
+        if a == goal or b == goal or c == goal:
+            path.append(state)
+            return path
 
-    
-    if b > 0:
-        
-        if a + b <= x:
-            if get_all_states((a + b, 0, c)):
-                ans.append(state)
-                return True
-        else:
-            if get_all_states((x, b - (x - a), c)):
-                ans.append(state)
-                return True
-
-        
-        if b + c <= z:
-            if get_all_states((a, 0, b + c)):
-                ans.append(state)
-                return True
-        else:
-            if get_all_states((a, b - (z - c), z)):
-                ans.append(state)
-                return True
-
-    
-    if c > 0:
-        
-        if a + c <= x:
-            if get_all_states((a + c, b, 0)):
-                ans.append(state)
-                return True
-        else:
-            if get_all_states((x, b, c - (x - a))):
-                ans.append(state)
-                return True
-
-        
-        if b + c <= y:
-            if get_all_states((a, b + c, 0)):
-                ans.append(state)
-                return True
-        else:
-            if get_all_states((a, y, c - (y - b))):
-                ans.append(state)
-                return True
-
-    return False
+        if state in visited:
+            continue
 
 
-initial_state = (12, 0, 0)
-print("Starting work...\n")
-get_all_states(initial_state)
-ans.reverse()  
-for i in ans:
-    print(i)
+        visited.append(state)
+        path.append(state)
+
+
+        # from A -> B
+        if a > 0 and b < 8:
+            transfer = min(a, 8-b)
+            queue.append((a - transfer, b+transfer, c))
+
+        # from A -> C
+        if a > 0 and b < 5:
+            transfer = min(a, 5 - c)
+            queue.append((a - transfer, b, c + transfer))
+
+        # from B -> A
+        if b > 0 and a < 12:
+            transfer = min(b, 12 - a)
+            queue.append((a + transfer, b - transfer, c))
+
+        # from B -> C
+        if b > 0 and c < 5:
+            transfer = min(b, 5 - c)
+            queue.append((a, b - transfer, c + transfer))
+
+        # from C -> A
+        if c > 0 and a < 12:
+            transfer = min(c, 12 - a)
+            queue.append((a + transfer, b, c - transfer))
+
+        # from C -> B
+        if c > 0 and b < 8:
+            transfer = min(c, 8 - b)
+            queue.append((a, c - transfer, b + transfer))
+
+    return None
+
+result = water_jug()
+
+if result:
+    for i in result:
+        print(i)
+else:
+    print("no solution found")
+
